cb6810e1658cdac3fe6ca5e8c9b5273754af28b1
Michelle M <michellemoon236@gmail.com>
2019-04-11 01:08:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+  return false if number < 2 
+  return true if number == 3 || number == 2 
+    if (2...number-1).any? do |i| 
+      number % i == 0
+    end
+      false
+    else
+      true
+    end
+end
